dee9e791327aa4a8df0c31ffa21f1e28f6d90537
Layonn Alves <layonn.alves@gmail.com>
2017-07-01 13:41:17 -0300

    Customize view, add seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Client.delete_all
+
+Client.create(
+        name:           'Layonn SA',
+        cnpj:           '123456789789',
+        sponsor:        'Layonn',
+        phone:          '999999999',
+        cell_phone:     '999999999',
+        email:          'layonn@example.com',
+        street:         'Elm',
+        address_number: '13',
+        city:           'Chesters Mill',
+        state:          'Maine'
+        )
